8b40f0bb180671de273d06f79ef27cc8940666b1
Gabriel Grego <gabrielsgrego@uol.com.br>
2024-08-14 00:36:53 -0300

    novo commit no branchTeste

--- ExercicioPratico.js
+++ ExercicioPratico.js
@@ -22,7 +22,7 @@ function encontrarMenorNumero(numeros){
     return menorNumero;
 }
 
-let numeros = [11,4,22,15,9,2,13,23];
+let numeros = [10,4,22,15,9,2,13,23];
 let maior = encontrarMaiorNumero(numeros);
 let menor = encontrarMenorNumero(numeros);
 
